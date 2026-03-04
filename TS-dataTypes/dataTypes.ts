5f166ca3a9a49836fd6a676654739b34b51f30b5
Suraj Rathour <110195289+SurajRathour24@users.noreply.github.com>
2026-03-04 23:45:39 +0530

    Array DT in TS

---
+++ TS-dataTypes/dataTypes.ts
@@ -0,0 +1,9 @@
+let num = 12;
+
+// This is what we have commonly used
+
+let arr1 = [1,2,3,4, "User", {"name" : "developer"}]   // Like JS
+
+// For strict type of data
+
+let marks: number[] = [1,2,3,4,5, "Marks"]  // Now this will strictly check
