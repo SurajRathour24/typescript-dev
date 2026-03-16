bf55e1b59604244fa93f1dba1276342f14099f79
Suraj Rathour <110195289+SurajRathour24@users.noreply.github.com>
2026-03-16 23:31:12 +0530

    Updated DT

---
+++ TS-dataTypes/primitive.ts
@@ -0,0 +1,15 @@
+// Tuples 
+
+let items: [number, string] = [1, "hey"];
+
+console.log(items);
+
+
+// enum are like objects 
+
+enum UserRoles {
+    Admin  = "Main Admin",
+    Guest = "Main Guest"
+}
+
+UserRoles.Admin;
